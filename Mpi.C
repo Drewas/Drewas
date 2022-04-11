766b92e97a7378500ea77d4eb39cccbff97c2aa5
Drewas <102230868+Drewas@users.noreply.github.com>
2022-04-11 19:25:53 +0300

    Update Mpi.C

--- Mpi.C
+++ Mpi.C
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <stdlib.h>
-#include <random>
 #include <math.h>
 
 using namespace std;
 
 void Mpi()
 {
-    setlocale( LC_ALL, "Russian" );
     double m_p0 = 0.135; //GeV/c^2//
     //|p1|=|p2|=p, E=pc => mc^2=2pc => p = mc^2/2= m/2//
     //m_po^2 = 4E_g1*E_g2 => E_g1 = m_po/2//
@@ -19,17 +17,17 @@ void Mpi()
     double E_g1, E_g2;
     E_g1 = m_p0/2;
     E_g2 = E_g1;
-    cout << "Импульс гамма кванта 1 = импульсу гамма кванта два и равен = " << p1 << " GeV/c" <<endl << "Энергия гамма кванта 1 = энергии гамма кванта два и равна = " << E_g1 << "GeV" << endl;
-    TCanvas *Pion = new TCanvas ("Pion","rand", 700,500);
+    cout << "Pgamma 1 = Pgamma 2 = " << p1 << " GeV/c" <<endl << "E gamma 1 = E gamma 2 = " << E_g1 << "GeV" << endl;
+    TCanvas *Pion = new TCanvas ("Pion","rand", 1920,1080);
     Pion -> Divide (3,2);
     Pion -> cd(1) ;
     TRandom *rnd = new TRandom();
     TH1F *hist_phi = new TH1F ("phi", "hist uniform phi",360,-TMath::Pi(),TMath::Pi());
-    TH1F *hist_px = new TH1F ("px", "hist uniform px",100,-TMath::Pi(),TMath::Pi());
-    TH1F *hist_py = new TH1F ("phi", "hist uniform px",100,-TMath::Pi(),TMath::Pi());
-    TH1F *hist_E = new TH1F ("E", "hist uniform E",100,-TMath::Pi(),TMath::Pi());
-    TH1F *hist_P = new TH1F ("P", "hist uniform P",100,-TMath::Pi(),TMath::Pi());
-    for (int i=0;i<=1000;i++)
+    TH1F *hist_px = new TH1F ("px", "hist uniform px",100,-0.08,0.08);
+    TH1F *hist_py = new TH1F ("py", "hist uniform py",100,-0.08,0.08);
+    TH1F *hist_E = new TH1F ("E", "hist uniform E",100,0.066,0.068);
+    TH1F *hist_P = new TH1F ("P", "hist uniform P",100,0.066,0.068);
+    for (int i=0;i<1000000;i++)
     {
         phi = rnd->Uniform(-1.*TMath::Pi(),TMath::Pi());
         px=p1*cos(phi);
@@ -56,3 +54,4 @@ void Mpi()
     Pion->SaveAs("/media/sf_Linux-To-Windows/Py.png");
 
 }
+
