d5f74d079db5a3af0df0fda5c1ea15b265707550
Drewas <102230868+Drewas@users.noreply.github.com>
2022-04-21 17:40:01 +0300

    Update Mpi.C

--- Mpi.C
+++ Mpi.C
@@ -9,18 +9,18 @@ void Mpi()
     double m_p0 = 0.135; //GeV/c^2//
     //|p1|=|p2|=p, E=pc => mc^2=2pc => p = mc^2/2= m/2//
     //m_po^2 = 4E_g1*E_g2 => E_g1 = m_po/2//
-    double dphi,phi1,phi2,px1,py1,pz1,theta1,eta1,cosinustheta1,px2,py2,pz2,theta2,eta2,cosinustheta2;
-    int const c = 1;
+    double dphi,phi1,px1,py1,pz1,theta1,eta1,cosinustheta1;
+    double phi2,px2,py2,pz2,theta2,eta2;
     double p1,p2;
     p1= m_p0/2;
     p2=p1;
     double E_g1, E_g2;
     E_g1 = m_p0/2;
     E_g2 = E_g1;
-    cout << "Pgamma 1 = Pgamma 2 = " << p1 << " GeV/c" <<endl << "E gamma 1 = E gamma 2 = " << E_g1 << "GeV" << endl;
+    cout << "P_gamma 1 = Pgamma 2 = " << p1 << " GeV/c" <<endl << "E_gamma 1 = E_gamma 2 = " << E_g1 << "GeV" << endl;
 
     TCanvas *Pion = new TCanvas ("Pion","rand", 2560,1440);
-    Pion -> Divide (6,3);
+    Pion -> Divide (5,5);
     Pion -> cd(1) ;
     TRandom *rnd = new TRandom();
     TH1D *hist_phi1 = new TH1D ("phi1", "hist phi1",360,-TMath::Pi(),TMath::Pi());
@@ -36,7 +36,6 @@ void Mpi()
 
     TH1D *hist_phi2 = new TH1D ("phi2", "hist phi2",360,-TMath::Pi(),TMath::Pi());
     TH1D *hist_eta2 = new TH1D ("eta2", "hist eta2",600,-3.,3.);
-    TH1D *hist_costhetha2 = new TH1D ("costheta2", "hist costheta2",100,-1.,1.);
     TH1D *hist_theta2 = new TH1D ("theta2", "hist theta2",180,0,TMath::Pi());
     TH1D *hist_px2 = new TH1D ("px2", "hist px2",100,-0.08,0.08);
     TH1D *hist_py2 = new TH1D ("py2", "hist py2",100,-0.08,0.08);
@@ -45,7 +44,8 @@ void Mpi()
     TH2D *hist2d2 = new TH2D("hist2d2","px2 and py2", 100,-0.08,0.08,100,-0.08,0.08);
     TH1D *hist_pz2 = new TH1D ("pz2", "hist pz2",100,-0.08,0.08);
 
-    TH1D *hist_dphi = new TH1D ("dphi", "phi1 - phi 2",360,-TMath::Pi(),TMath::Pi());
+    TH1D *hist_dphi = new TH1D ("dphi", "phi1 - phi 2",360,-2.*TMath::Pi(),0);
+    TH2D *hist3d1 = new TH2D("hist2d1","px1 and py1", 100,-0.08,0.08,100,-0.08,0.08);
 
     for (int i=0;i<1000000;i++)
     {
@@ -83,6 +83,7 @@ void Mpi()
         }
 
         hist_phi1->Fill(phi1);
+        hist_costhetha1->Fill(cosinustheta1);
         hist_theta1->Fill(theta1);
         hist_px1->Fill(px1);
         hist_py1->Fill(py1);
@@ -103,8 +104,11 @@ void Mpi()
         hist_eta2->Fill(eta2);
 
         hist_dphi->Fill(dphi);
+        hist3d1->Fill(px1,py1);
     }
     hist_phi1->Draw();
+    Pion -> cd(10);
+    hist_costhetha1->Draw();
     Pion -> cd(2);
     hist_E1-> Draw();
     Pion -> cd(3);
@@ -121,26 +125,30 @@ void Mpi()
     hist_theta1->Draw();
     Pion->cd(9);
     hist_eta1->Draw();
-    Pion->SaveAs("/media/sf_Linux-To-Windows/eta.png");
+    Pion->SaveAs("/media/sf_Linux-To-Windows/part 1.png");
 
-    hist_phi2->Draw();
-    Pion -> cd(10);
-    hist_E2-> Draw();
     Pion -> cd(11);
+    hist_phi2->Draw();
+    Pion -> cd(12);
+    hist_E2-> Draw();
+    Pion -> cd(13);
     hist_P2 -> Draw();
-    Pion->cd(12);
-    hist_px2 ->Draw();
-    Pion->cd(13);
-    hist_py2 ->Draw();
     Pion->cd(14);
-    hist_pz2 ->Draw();
-    Pion->cd(15)->SetLogz();
-    hist2d2->Draw("colz");
+    hist_px2 ->Draw();
+    Pion->cd(15);
+    hist_py2 ->Draw();
     Pion->cd(16);
-    hist_theta2->Draw();
-    Pion->cd(17);
-    hist_eta2->Draw();
+    hist_pz2 ->Draw();
+    Pion->cd(17)->SetLogz();
+    hist2d2->Draw("colz");
     Pion->cd(18);
+    hist_theta2->Draw();
+    Pion->cd(19);
+    hist_eta2->Draw();
+    Pion->cd(20);
     hist_dphi->Draw();
+    Pion->cd(21);
+    gStyle->SetPalette(1);
+    hist3d1->Draw("SURF1");
     Pion->SaveAs("/media/sf_Linux-To-Windows/all.png");
 }
